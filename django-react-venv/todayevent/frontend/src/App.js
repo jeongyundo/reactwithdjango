2e38ed5152e04a9ea1227c406f0c539cb2e26bba
정윤도 <truenorthj@gmail.com>
2019-08-25 22:14:55 +0900

    added redux, redux-devtools-extention and redux-thunk and edited js files

--- django-react-venv/todayevent/frontend/src/App.js
+++ django-react-venv/todayevent/frontend/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import App6 from './App6';
 
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component{
 
     render() {
         return (
             <Router>
-                <div>
-                    <Route exact path="/" component={App6}/>
-                    <Route exact path="/App6" component={App6}/>
-                </div>
+                <Provider store={store}>
+                    <div>
+                        <Route exact path="/" component={App6}/>
+                        <Route exact path="/App6" component={App6}/>
+                    </div>
+                </Provider>
             </Router>
         );
     }
