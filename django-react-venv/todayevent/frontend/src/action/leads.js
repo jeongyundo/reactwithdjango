975e1d0c83b280c52571a539ccc98f7957ac9801
정윤도 <truenorthj@gmail.com>
2019-08-26 22:34:49 +0900

    add post and delete system

--- django-react-venv/todayevent/frontend/src/action/leads.js
+++ django-react-venv/todayevent/frontend/src/action/leads.js
@@ -7,10 +7,40 @@ import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 // GET LEADS
 export const getLeads = () => (dispatch, getState) => {
   axios
-    .get("/api/leads/", tokenConfig(getState))
+    .get("/infoapi/", tokenConfig(getState))
     .then(res => {
       dispatch({
         type: GET_LEADS,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+// DELETE LEAD
+export const deleteLead = id => (dispatch, getState) => {
+  axios
+    .delete(`/infoapi/${id}/`, tokenConfig(getState))
+    .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// ADD LEAD
+export const addLead = lead => (dispatch, getState) => {
+  axios
+    .post("/infoapi/", lead, tokenConfig(getState))
+    .then(res => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
+      dispatch({
+        type: ADD_LEAD,
         payload: res.data
       });
     })
